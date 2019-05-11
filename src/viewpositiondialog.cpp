eeb11664ff7fe51d4a04cea16812a287f00ed1b9
BenjaminDeguerre <benjamin.deguerre@insa-rouen.fr>
2019-05-11 17:47:48 +0200

    updating some names

--- src/viewpositiondialog.cpp
+++ src/viewpositiondialog.cpp
@@ -19,13 +19,13 @@ void viewPositionDialog::on_pushButton_clicked()
     float eyex = ui->lineEdit->text().toFloat();
     float eyey = ui->lineEdit_2->text().toFloat();
     float eyez = ui->lineEdit_3->text().toFloat();
-    float directx = ui->lineEdit_4->text().toFloat();
-    float directy = ui->lineEdit_5->text().toFloat();
-    float directz = ui->lineEdit_6->text().toFloat();
+    float x = ui->lineEdit_4->text().toFloat();
+    float y = ui->lineEdit_5->text().toFloat();
+    float z = ui->lineEdit_6->text().toFloat();
 
     QMatrix4x4 mview;
     mview.lookAt(QVector3D(eyex, eyey, eyez),
-                 QVector3D(eyex + directx, eyey + directy, eyez + directz),
+                 QVector3D(x, y, z),
                  QVector3D(0.0,1.0,0.0));
     view->setView(mview);
     this->done(0);
